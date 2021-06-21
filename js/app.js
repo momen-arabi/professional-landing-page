4675372a258c33472e4516d466f0f62766d8457c
momen-arabi <64374794+momen-arabi@users.noreply.github.com>
2021-06-21 23:44:44 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -17,24 +17,6 @@
  * Define Global Variables
  *
 */
-let menuList = document.querySelectorAll("section");
-const fragment = document.createDocumentFragment();
-const navBar = document.getElementById("navbar__list");
-
-for (let i = 0; i < menuList.length; i++) {
-  let menuItem = document.createElement("li");
-  menuItem.classList.add("menu__link");
-  let menuAnchor = menuList[i];
-  menuItem.innerText = menuList[i].getAttribute("data-nav");
-  menuItem.addEventListener("click", function() {
-    menuAnchor.scrollIntoView();
-  });
-  fragment.appendChild(menuItem);
-  // console.log(menuList[i].getAttribute("data-nav"));
-}
-
-navBar.appendChild(fragment);
-
 
 
 
@@ -53,6 +35,55 @@ navBar.appendChild(fragment);
 */
 
 // build the nav
+const sections = document.querySelectorAll("section");
+const fragment = document.createDocumentFragment();
+const navBar = document.getElementById("navbar__list");
+
+for (let i = 0; i < sections.length; i++) {
+  let menuItem = document.createElement("li");
+  // let menuAnchor = sections[i];
+  menuItem.classList.add("menu__link");
+  menuItem.innerText = sections[i].getAttribute("data-nav");
+  menuItem.addEventListener("click", function() {
+    sections[i].scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    let section = menuItem.innerText;
+    console.log(section);
+    // let sectionTop = sections[i].getBoundingClientRect().top;
+    // console.log(sectionTop);
+  });
+  fragment.appendChild(menuItem);
+  // console.log(menuList[i].getAttribute("data-nav"));
+}
+
+navBar.appendChild(fragment);
+
+
+let navItems = document.querySelectorAll("#navbar__list li");
+navItems.forEach((elm, i, arr) => {
+  elm.addEventListener("click", () => {
+    if (elm === )
+  })
+});
+
+// let section = document.querySelector("section");
+// let section1Item = document.querySelector(".navbar__menu li");
+// section1Item.addEventListener("click", () => {
+//   section.scrollIntoView();
+//   let sectionTop = section.getBoundingClientRect().top;
+//   console.log(sectionTop);
+// })
+
+// console.log(sectionTop);
+// if (sectionTop <= 0 && sectionTop >= -250) {
+//   alert('Holaaaaaaaaaaa');
+// };
+
+// sections.forEach((elem, i, arr) => {
+//   let sectionTop = elem.getBoundingClientRect().top;
+//   if (sectionTop >= -250 && sectionTop <= 0) {
+//     elem.style.backgroundColor = "red";
+//   }
+// });
 
 
 // Add class 'active' to section when near top of viewport

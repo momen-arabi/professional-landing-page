dad65dc4bc8596d2aa78ce0eefeae85b78bab437
momen-arabi <64374794+momen-arabi@users.noreply.github.com>
2021-06-22 23:24:26 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -17,89 +17,71 @@
  * Define Global Variables
  *
 */
-
-
-
+const sections = document.querySelectorAll("section");
+const fragment = document.createDocumentFragment();
+const navBar = document.getElementById("navbar__list");
 /**
  * End Global Variables
  * Start Helper Functions
  *
 */
-
-
-
 /**
  * End Helper Functions
  * Begin Main Functions
  *
 */
 
-// build the nav
-const sections = document.querySelectorAll("section");
-const fragment = document.createDocumentFragment();
-const navBar = document.getElementById("navbar__list");
-
-for (let i = 0; i < sections.length; i++) {
-  let menuItem = document.createElement("li");
-  // let menuAnchor = sections[i];
-  menuItem.classList.add("menu__link");
-  menuItem.innerText = sections[i].getAttribute("data-nav");
-  menuItem.addEventListener("click", function() {
-    sections[i].scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-    let section = menuItem.innerText;
-    console.log(section);
-    // let sectionTop = sections[i].getBoundingClientRect().top;
-    // console.log(sectionTop);
+// Build The Nav
+sections.forEach((elm) => {
+  let menuItem = document.createElement("li"); //create li elements
+  menuItem.classList.add("menu__link"); //adding menu__link class to each menu item
+  menuItem.innerText = elm.getAttribute("data-nav"); //naming menu items
+  elm.classList.remove("your-active-class"); //removing any active section styling
+  menuItem.addEventListener("click", () => {
+    elm.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"}); //scrolling to the corresponding section
   });
-  fragment.appendChild(menuItem);
-  // console.log(menuList[i].getAttribute("data-nav"));
-}
-
-navBar.appendChild(fragment);
+  fragment.appendChild(menuItem); //adding all menu item li elements to the fragment
+});
+navBar.appendChild(fragment); //adding the list items to the navbar ul
 
 
-let navItems = document.querySelectorAll("#navbar__list li");
-navItems.forEach((elm, i, arr) => {
-  elm.addEventListener("click", () => {
-    if (elm === )
-  })
+// Active Section Styling
+let menuItems = document.querySelectorAll("#navbar__list li");
+
+window.addEventListener("scroll", () => {
+  sections.forEach((elm) => {
+    let sectionTop = elm.getBoundingClientRect().top;
+    sectionTop <= 100 && sectionTop >= -250 ? elm.classList.add("your-active-class") : elm.classList.remove("your-active-class");
+    if (elm.classList.contains("your-active-class")) {
+      console.log(elm.getAttribute("data-nav"));
+    }
+  });
 });
 
-// let section = document.querySelector("section");
-// let section1Item = document.querySelector(".navbar__menu li");
-// section1Item.addEventListener("click", () => {
-//   section.scrollIntoView();
-//   let sectionTop = section.getBoundingClientRect().top;
-//   console.log(sectionTop);
-// })
 
-// console.log(sectionTop);
-// if (sectionTop <= 0 && sectionTop >= -250) {
-//   alert('Holaaaaaaaaaaa');
-// };
-
-// sections.forEach((elem, i, arr) => {
-//   let sectionTop = elem.getBoundingClientRect().top;
-//   if (sectionTop >= -250 && sectionTop <= 0) {
-//     elem.style.backgroundColor = "red";
-//   }
-// });
-
+//Active Navbar Items
+window.addEventListener("scroll", () => {
+  menuItems.forEach((item) => {
+    sections.forEach((sec) => {
+      if (sec.classList.contains("your-active-class")) {
+        if (sec.getAttribute("data-nav") === item.innerText) {
+          item.classList.add("active__link");
+        }
+        else {
+          item.classList.remove("active__link");
+        }
+      }
+    })
+  });
+})
 
 // Add class 'active' to section when near top of viewport
-
-
 // Scroll to anchor ID using scrollTO event
-
-
 /**
  * End Main Functions
  * Begin Events
  *
 */
-
 // Build menu
-
 // Scroll to section on link click
-
 // Set sections as active
